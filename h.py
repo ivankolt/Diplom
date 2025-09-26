915b517b1a0c08de64599e842966565bb3877b5d
ivankolt <va.koltsov@mail.ru>
2025-09-26 16:27:30 +0500

    вапва

--- h.py
+++ h.py
@@ -25,7 +25,9 @@ CLASSES = [
     "train",
     "tvmonitor",
 ]
-net = cv2.dnn.readNetFromCaffe("deploy.prototxt", "MobileNetSSD_deploy.caffemodel")
+net = cv2.dnn.readNetFromCaffe(
+    "MobileNetSSD_deploy.prototxt", "MobileNetSSD_deploy.caffemodel"
+)
 
 cap = cv2.VideoCapture("https://cams.is74.ru/live/main/cam1026.m3u8")
 while True:
@@ -33,7 +35,7 @@ while True:
     if not ret:
         break
 
-    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300,300), 127.5)
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
     net.setInput(blob)
     detections = net.forward()
 
@@ -41,21 +43,29 @@ while True:
         confidence = detections[0, 0, i, 2]
         if confidence > 0.4:
             idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([
-                frame.shape[1], frame.shape[0],
-                frame.shape[1], frame.shape[0]
-            ])
+            box = detections[0, 0, i, 3:7] * np.array(
+                [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]]
+            )
             (x1, y1, x2, y2) = box.astype("int")
             label = CLASSES[idx]
             if label in ["car", "bus", "truck"]:
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, label, (x1, y1-10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+                cv2.putText(
+                    frame,
+                    label,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    2,
+                )
     cv2.imshow("Detection", frame)
+
+    time.sleep(0.01)
+
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-    time.sleep(0.031)
 
 cap.release()
 cv2.destroyAllWindows()
